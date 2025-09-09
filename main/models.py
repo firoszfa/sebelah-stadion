29b942bf667552477e583d438d968f6ec5e2c710
firoszfa <firoszufa20@gmail.com>
2025-09-09 23:01:59 +0700

    tugas 1

--- main/models.py
+++ main/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Products(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=255)
     is_featured = models.BooleanField(default=False)
